e10d0c965d4e046164935cee2c3d276ac994464f
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2023-11-21 21:49:10 +0200

    Pytohn practice

---
+++ PRO 511 & 512/concate.py
@@ -0,0 +1,9 @@
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+list3 = list1 + list2
+print(list3)
+
+nam1 = ['Jenny', 'Chloe']
+nam2 = ['Karen', 'Aubrey']
+nam1 += nam2
+print(nam1)
---
+++ PRO 511 & 512/del.py
@@ -0,0 +1,3 @@
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+del my_list[2]
+print(my_list)
