6b22987d29144d9779742c139ef846fa2b83aa28
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2023-11-22 20:37:28 +0200

    classes in python

--- PRO 511 & 512/class lesson/bank_account.py
+++ PRO 511 & 512/class lesson/bank_account.py
@@ -1,25 +1,37 @@
 class BankAccount:
+
     def __init__(self):
         self.__balance = 0
 
     def deposit(self, bal):
+
         self.__balance += bal
 
         return self.__balance
     
     def withdraw(self, bal):
 
-        if bal <= self.__balance:
-            self.__balance -= bal
-        else:
-            self.__balance = "Insufficient funds"
+        if self.__balance >= bal:
 
-        return f"You remaining balance is {self.__balance}"
-    
+            self.__balance -= bal
+            return f"You remaining balance is {self.__balance}"
+        else:
+            return "Insufficient funds"
+          
 
 money_in = float(input("Enter the amount you like to deposit: "))
 money_out = float(input("Enter the amount you like to withdraw: "))
 
-deposit = BankAccount()
-print(deposit.deposit(money_in))
-print(deposit.withdraw(money_out))
+my_account = BankAccount()
+print(my_account.deposit(money_in))
+print(my_account.withdraw(money_out))
+
+# amount = 1
+
+# def deposit(bal):
+#     global amount
+
+#     amount += bal
+#     print(amount)
+
+# deposit(1000)
